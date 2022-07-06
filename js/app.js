a4d040cc0a0094d1b46a9c5cd83d834549528ca8
Makarov <academypro2021@gmail.com>
2022-07-06 21:43:53 +0300

    Убрал  Collapse

--- js/app.js
+++ js/app.js
@@ -51,15 +51,15 @@
 
     // Collapse
 
-    $("[data-collapse]").on("click", function(event) {
-      event.preventDefault();
+    // $("[data-collapse]").on("click", function(event) {
+    //   event.preventDefault();
 
-      var $this = $(this),
-          blockId = $this.data("collapse");
+    //   var $this = $(this),
+    //       blockId = $this.data("collapse");
 
-          $this.toggleClass("active");
-          $(blockId).slideToggle();
-    });
+    //       $this.toggleClass("active");
+    //       $(blockId).slideToggle();
+    // });
 
     // Slider
 
