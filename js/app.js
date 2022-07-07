890c20eee937e8265ee2ce2249e2480650c7810a
Makarov <academypro2021@gmail.com>
2022-07-08 02:38:08 +0300

    Обновление

--- js/app.js
+++ js/app.js
@@ -49,27 +49,4 @@
 
     });
 
-    // Collapse
-
-    // $("[data-collapse]").on("click", function(event) {
-    //   event.preventDefault();
-
-    //   var $this = $(this),
-    //       blockId = $this.data("collapse");
-
-    //       $this.toggleClass("active");
-    //       $(blockId).slideToggle();
-    // });
-
-    // Slider
-
-    $("[data-slider]").slick({
-      infinite: true,
-      fade: false,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
-
-
-
  });
